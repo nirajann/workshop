3644233cf653a3006fe847bcd42f36dcf58afa7e
Nirajan <gnirajan46@gmail.com>
2021-02-23 12:27:47 +0545

    initial comit

---
+++ lab1/exercise3.py
@@ -0,0 +1,14 @@
+"""
+n students takes k apples and distribute among them evenly the remaining (the individial) part remain in the basket
+how many apples will each single student get? how many apple ramin in the basket ?the program reads the number N and K
+it should print two anser for the question above
+"""
+apple = int(input("enter the number of apple"))
+student = int(input("enter the number of  student"))
+
+c = (apple//student)
+d = (apple%student)
+
+print(f"the student got{c}")
+print(f"the remaining apples are {d}")
+
