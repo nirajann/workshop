dbc73a393320fe2d1177e408b6e8e55070ba218b
Nirajan <gnirajan46@gmail.com>
2021-02-23 11:17:33 +0545

    Initial commit

---
+++ lab1/exercise.py
@@ -0,0 +1,9 @@
+#write a program that takes three number and print their sum.every number is given in seperate line
+
+a = int(input("enter number "))
+b = int(input("enter number "))
+c = int(input("enter number "))
+
+sum =  a + b + c
+
+print(f"the sum is {sum} ")
