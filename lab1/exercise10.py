69b2139468288331312523d4c1591bf83c73323d
Nirajan <gnirajan46@gmail.com>
2021-03-09 12:28:55 +0545

    initial commit

---
+++ lab1/exercise10.py
@@ -0,0 +1,12 @@
+"""
+write a python program to  convert seconds to day ,hour ,minute and second
+
+"""
+
+seconds = int(input("enter seconds"))
+
+minute = seconds // 60
+hour = minute // 60
+day = hour // 24
+
+print(f"  {day} days : {hour} hours : {minute} minutes : {seconds} seconds")
