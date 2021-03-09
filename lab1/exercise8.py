e79726edf7e31bcd5fbc00cbc80feff901dd8486
Nirajan <gnirajan46@gmail.com>
2021-03-09 12:00:02 +0545

    initial commit

---
+++ lab1/exercise8.py
@@ -0,0 +1,11 @@
+"""
+write the python program which  accepts  the radious of a circle from the user and complete  the area .
+area of circle = pi * r **2
+"""
+
+radious_of_circle =  int(input("enter the raidous of circle"))
+pi = 3.14
+radious_square = radious_of_circle ** 2
+area_of_circle = pi * radious_square
+
+print(f"the radious of circle is {area_of_circle}")
